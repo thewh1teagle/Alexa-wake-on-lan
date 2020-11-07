d1a492adeb2c3645fb55ba249c5c5d184941b1d0
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2020-11-07 17:56:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
-from sinric import Sinric
-from wakeonlan import send_magic_packet
+import websocket
+import time
+import base64
+import json
+from wakeonlan import send_magic_packet as power_on_pc
 
-apiKey = 'YourApiKey'  # https://sinric.com
-pc_mac = 'aa:bb:cc:dd:ee:ff' # The interface mac address of the pc
-device_id = "YourDeviceId" # Grab it from https://sinric.com too
+api_key = '<api_key>'  # https://sinric.com
+pc_mac = 'aa:bb:cc:dd:ee:ff'
+device_id = "<device_id>"
+
+def on_message(ws, message):
+    obj = json.loads(message)
+    deviceId = obj['deviceId']
+    action = obj['action']
+    value = obj['value']
+    if deviceId == device_id and value == "ON":
+        power_on_pc(pc_mac)
+    print(message)
 
 
-def power_state(deviceId, state):
-    if deviceId == device_id and state == 'ON':
-        send_magic_packet(pc_mac)
+def on_error(ws, error):
+    print(error)
 
+
+def on_close(ws):
+    print("### closed ###")
+    time.sleep(2)
+    initiate()
+
+
+def on_open(ws):
+    print("### Initiating new websocket connection ###")
+
+
+def initiate():
+    raw_api_key = base64.b64encode(f'apikey:{api_key}'.encode('ascii')).decode("ascii")
+    websocket.enableTrace(True)
+
+    ws = websocket.WebSocketApp("ws://iot.sinric.com",
+            header={'Authorization:' + raw_api_key},
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+
+    ws.run_forever()
 
 
 if __name__ == "__main__":
-    callbacks = {"setPowerState": power_state}    
-    app = Sinric(apiKey, callbacks)
     print("Alexa wake on lan started...")
-    app.handle()
-    
+    initiate()
